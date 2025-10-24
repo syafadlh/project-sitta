a656032337050f9f85c05edde4e2cf4ce110c100
= <=>
2025-10-24 07:59:04 +0700

    feat: buat halaman dashboard dengan menu navigasi & greeting otomatis

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,19 @@
-// ============ LOGIN ============
+// Fungsi login (langsung masuk dashboard tanpa validasi)
 function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if (email === "admin@ut.ac.id" && password === "admin123") {
-    alert("Login berhasil!");
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Email/password yang Anda masukkan salah");
-  }
+  // langsung pindah ke dashboard
+  window.location.href = "dashboard.html";
+}
+
+
+// ======= GREETING OTOMATIS =======
+if (document.getElementById("greeting")) {
+  const jam = new Date().getHours();
+  let sapaan = "Selamat Malam";
+  if (jam < 12) sapaan = "Selamat Pagi";
+  else if (jam < 15) sapaan = "Selamat Siang";
+  else sapaan = "Selamat Sore";
+  document.getElementById("greeting").textContent = sapaan;
 }
